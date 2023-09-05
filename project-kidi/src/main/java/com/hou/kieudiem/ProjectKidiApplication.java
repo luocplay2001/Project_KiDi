0683cb16abc57cea711887b17e60a30e27cae5df
NguyenKien <74364366+luocplay2001@users.noreply.github.com>
2023-09-05 10:52:51 +0700

    update

---
+++ project-kidi/src/main/java/com/hou/kieudiem/ProjectKidiApplication.java
@@ -0,0 +1,13 @@
+package com.hou.kieudiem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKidiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectKidiApplication.class, args);
+    }
+
+}
---
+++ project-kidi/src/test/java/com/hou/kieudiem/ProjectKidiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hou.kieudiem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKidiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
